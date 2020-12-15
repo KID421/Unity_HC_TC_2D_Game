6c2c77648a2b92301ab6055b75c3e437a3e250c3
KID <50666207+KID421@users.noreply.github.com>
2020-12-15 21:12:21 +0800

    延遲呼叫

--- 2DGame/Assets/Scripts/MenuManager.cs
+++ 2DGame/Assets/Scripts/MenuManager.cs
@@ -5,9 +5,27 @@ public class MenuManager : MonoBehaviour
 {
     // public 公開，允許按鈕呼叫
     /// <summary>
+    /// 延遲呼叫，等音效播完
+    /// </summary>
+    public void DelayStartGame()
+    {
+        // 延遲呼叫("方法名稱"，延遲秒數)；
+        // Invoke();
+        Invoke("StartGame", 0.9f);
+    }
+
+    /// <summary>
+    /// 延遲呼叫，等音效播完
+    /// </summary>
+    public void DelayQuitGame()
+    {
+        Invoke("QuitGame", 0.9f);
+    }
+
+    /// <summary>
     /// 開始遊戲
     /// </summary>
-    public void StartGame()
+    private void StartGame()
     {
         // 載入指定場景
         // 場景管理器 的 載入場景("場景名稱")
@@ -18,7 +36,7 @@ public class MenuManager : MonoBehaviour
     /// <summary>
     /// 離開遊戲
     /// </summary>
-    public void QuitGame()
+    private void QuitGame()
     {
         // 退出遊戲
         // 應用程式 的 離開遊戲()
