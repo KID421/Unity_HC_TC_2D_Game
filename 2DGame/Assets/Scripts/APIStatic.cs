451331bec0e2c638d4055167089326c2cc550241
KID <50666207+KID421@users.noreply.github.com>
2020-12-15 20:28:55 +0800

    示範練習使用靜態方法

--- 2DGame/Assets/Scripts/APIStatic.cs
+++ 2DGame/Assets/Scripts/APIStatic.cs
@@ -11,7 +11,7 @@ public class APIStatic : MonoBehaviour
     /// </summary>
     private void Start()
     {
-        // 靜態屬性
+        // 靜態屬性 Static Properties
         // 取得
         // 語法：類別名稱.靜態屬性名稱
         print(Mathf.PI);
@@ -25,6 +25,20 @@ public class APIStatic : MonoBehaviour
         // 練習
         print("所有攝影機的數量：" + Camera.allCamerasCount);
         Cursor.visible = false;
+
+        // 靜態方法 Static Methods
+        // 使用
+        // 語法：類別名稱.靜態方法名稱(對應的參數)
+        int number = Mathf.Abs(-999);
+        print("取得絕對值後的結果：" + number);
+
+        print("隨機範圍 3 - 20.5：" + Random.Range(3.0f, 20.5f));
+        print("隨機 1 - 100：" + Random.Range(1, 100));
+
+        // 練習
+        // Application.OpenURL("https://unity.com/");
+
+        print("7.7 去小數點：" + Mathf.Floor(7.7f));
     }
 
     /// <summary>
@@ -32,7 +46,8 @@ public class APIStatic : MonoBehaviour
     /// </summary>
     private void Update()
     {
-        print("是否按任意鍵：" + Input.anyKey);
-        print("遊戲時間：" + Time.time);
+        // print("是否按任意鍵：" + Input.anyKey);
+        // print("遊戲時間：" + Time.time);
+        print("是否按下空白鍵：" + Input.GetKeyDown("space"));
     }
 }
