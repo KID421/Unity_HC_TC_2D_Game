97862b115ad038648157d8aba5d114a4c4a52527
KID <50666207+KID421@users.noreply.github.com>
2020-12-15 21:00:50 +0800

    按鈕：呼叫公開方法的設定

--- 2DGame/Assets/Scripts/MenuManager.cs
+++ 2DGame/Assets/Scripts/MenuManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    // public 公開，允許按鈕呼叫
     /// <summary>
     /// 開始遊戲
     /// </summary>
@@ -10,6 +11,7 @@ public class MenuManager : MonoBehaviour
     {
         // 載入指定場景
         // 場景管理器 的 載入場景("場景名稱")
+        // 場景管理器 的 載入場景(1)
         SceneManager.LoadScene("遊戲場景");
     }
 
