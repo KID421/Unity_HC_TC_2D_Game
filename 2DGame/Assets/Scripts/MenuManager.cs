7d2908d42d8bce90bd329c966cf88ee6b8be7073
KID <50666207+KID421@users.noreply.github.com>
2020-12-15 20:47:34 +0800

    應用：使用靜態方法處理載入遊戲

--- 2DGame/Assets/Scripts/MenuManager.cs
+++ 2DGame/Assets/Scripts/MenuManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
@@ -7,7 +8,9 @@ public class MenuManager : MonoBehaviour
     /// </summary>
     public void StartGame()
     {
-
+        // 載入指定場景
+        // 場景管理器 的 載入場景("場景名稱")
+        SceneManager.LoadScene("遊戲場景");
     }
 
     /// <summary>
@@ -15,6 +18,8 @@ public class MenuManager : MonoBehaviour
     /// </summary>
     public void QuitGame()
     {
-
+        // 退出遊戲
+        // 應用程式 的 離開遊戲()
+        Application.Quit();
     }
 }
