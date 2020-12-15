d90c61262697ea872e894d5e7442aef1ae5d8990
KID <50666207+KID421@users.noreply.github.com>
2020-12-15 19:46:48 +0800

    練習與示範

--- 2DGame/Assets/Scripts/APIStatic.cs
+++ 2DGame/Assets/Scripts/APIStatic.cs
@@ -16,5 +16,23 @@ public class APIStatic : MonoBehaviour
         // 語法：類別名稱.靜態屬性名稱
         print(Mathf.PI);
         print(Mathf.Infinity);
+
+        // 設定
+        // 如果有 Read Only 代表不能設定
+        // 語法：類別名稱.靜態屬性名稱 = 相同類型的值
+        Time.timeScale = 0.5f;
+
+        // 練習
+        print("所有攝影機的數量：" + Camera.allCamerasCount);
+        Cursor.visible = false;
+    }
+
+    /// <summary>
+    /// 更新事件：一秒執行約 60 次 (60FPS)
+    /// </summary>
+    private void Update()
+    {
+        print("是否按任意鍵：" + Input.anyKey);
+        print("遊戲時間：" + Time.time);
     }
 }
