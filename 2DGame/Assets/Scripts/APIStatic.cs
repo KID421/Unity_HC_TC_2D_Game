19b9c0e8b57205fcfc0aed40d7cec17b702ffac9
KID <50666207+KID421@users.noreply.github.com>
2020-12-15 19:12:41 +0800

    取得靜態屬性

---
+++ 2DGame/Assets/Scripts/APIStatic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 認識靜態的 API
+/// 靜態 static
+/// </summary>
+public class APIStatic : MonoBehaviour
+{
+    /// <summary>
+    /// 開始事件：播放後執行一次
+    /// </summary>
+    private void Start()
+    {
+        // 靜態屬性
+        // 取得
+        // 語法：類別名稱.靜態屬性名稱
+        print(Mathf.PI);
+        print(Mathf.Infinity);
+    }
+}
