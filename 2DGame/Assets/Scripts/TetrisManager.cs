f7b0d998e25f2c6f68198949ce903c4176c16ed6
KID <50666207+KID421@users.noreply.github.com>
2020-12-17 20:56:15 +0800

    俄羅斯方塊生成

--- 2DGame/Assets/Scripts/TetrisManager.cs
+++ 2DGame/Assets/Scripts/TetrisManager.cs
@@ -17,6 +17,8 @@ public class TetrisManager : MonoBehaviour
     public AudioClip soundMove;
     public AudioClip soundClear;
     public AudioClip soundLose;
+    [Header("下一個俄羅斯方塊區域")]
+    public Transform traNextArea;
 
     /// <summary>
     /// 下一顆俄羅斯方塊編號
@@ -36,6 +38,22 @@ public class TetrisManager : MonoBehaviour
     {
         // 下一顆編號 = 隨機 的 範圍(最小，最大) - 整數不會等於最大值
         indexNext = Random.Range(0, 7);
+        // 下一個俄羅斯方塊區域 . 取得子物件(子物件編號) . 轉為遊戲物件 . 啟動設定(顯示)
+        traNextArea.GetChild(indexNext).gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// 開始遊戲
+    /// 1. 生成俄羅斯方塊要放在正確位置
+    /// 2. 上一次俄羅斯方塊隱藏
+    /// 3. 隨機取得下一個
+    /// </summary>
+    public void StartGame()
+    {
+        // 保存上一次的俄羅斯方塊
+        GameObject tetris = traNextArea.GetChild(indexNext).gameObject;
+        // 生成物件(物件)
+        Instantiate(tetris);
     }
 
     /// <summary>
