c066340fb20ae710beb8f768ee019589b3ec8d92
KID <50666207+KID421@users.noreply.github.com>
2020-12-24 21:12:56 +0800

    添加俄羅斯方塊檢查射線

---
+++ 2DGame/Assets/Scripts/Tetris.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class Tetris : MonoBehaviour
+{
+    [Header("角度為零，線條的長度")]
+    public float length0;
+    [Header("角度為九十度，線條的長度")]
+    public float length90;
+
+    /// <summary>
+    /// 紀錄目前長度
+    /// </summary>
+    public float length;
+
+    /// <summary>
+    /// 繪製圖示
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        // 圖示 的 顏色
+        Gizmos.color = Color.red;
+
+        // 將浮點數角度 轉為 整數 - 去小數點
+        int z = (int)transform.eulerAngles.z;
+
+        if (z == 0 || z == 180)
+        {
+            // 儲存目前長度
+            length = length0;
+            // 圖示 的 繪製射線(起點，方向 * 長度)
+            Gizmos.DrawRay(transform.position, Vector3.right * length0);
+        }
+        else if (z == 90 || z == 270)
+        {
+            // 儲存目前長度
+            length = length90;
+            Gizmos.DrawRay(transform.position, Vector3.right * length90);
+        }
+    }
+
+    private void Start()
+    {
+        // 儲存遊戲開始的角度
+        length = length0;
+    }
+
+    private void Update()
+    {
+        CheckWall();
+    }
+
+    /// <summary>
+    /// 是否碰到右邊牆壁
+    /// </summary>
+    public bool wallRight;
+
+    /// <summary>
+    /// 檢查射線是否碰到牆壁
+    /// </summary>
+    private void CheckWall()
+    {
+        // 2D 物理碰撞資訊 區域變數名稱 = 2D 物理.射線碰撞(起點，方向，長度，圖層)
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.right, length, 1 << 8);
+
+        // 並且 &&
+        // 如果 碰到東西 並且 名稱 為 牆壁：右邊
+        if (hit && hit.transform.name == "牆壁：右邊")
+        {
+            wallRight = true;
+        }
+        else
+        {
+            wallRight = false;
+        }
+    }
+}
--- 2DGame/Assets/Scripts/TetrisManager.cs
+++ 2DGame/Assets/Scripts/TetrisManager.cs
@@ -21,6 +21,17 @@ public class TetrisManager : MonoBehaviour
     public Transform traNextArea;
     [Header("畫布")]
     public Transform traCanvas;
+    [Header("生成的起始位置")]
+    public Vector2[] posSpawn =
+    {
+        new Vector2(20, 400),
+        new Vector2(20, 360),
+        new Vector2(0, 360),
+        new Vector2(0, 380),
+        new Vector2(0, 380),
+        new Vector2(20, 360),
+        new Vector2(20, 360)
+    };
 
     /// <summary>
     /// 下一顆俄羅斯方塊編號
@@ -62,7 +73,7 @@ public class TetrisManager : MonoBehaviour
             if (timer >= timeFall)
             {
                 timer = 0;
-                currentTetris.anchoredPosition -= new Vector2(0, 50);
+                currentTetris.anchoredPosition -= new Vector2(0, 40);
             }
 
             #region 控制俄羅斯方塊 左右、旋轉與加速
@@ -73,21 +84,26 @@ public class TetrisManager : MonoBehaviour
                 // 按下 D 或者 右鍵 往右 50 
                 if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
                 {
-                    currentTetris.anchoredPosition += new Vector2(20, 0);
+                    currentTetris.anchoredPosition += new Vector2(40, 0);
                 }
             }
-            
-            // 按下 A 或者 左鍵 往右 50 
-            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            // 如果 X 座標 小於 280 才能往右移動
+            if (currentTetris.anchoredPosition.x > -280)
             {
-                currentTetris.anchoredPosition -= new Vector2(20, 0);
+                // 或者 ||
+                // 按下 D 或者 右鍵 往右 50 
+                if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    currentTetris.anchoredPosition -= new Vector2(40, 0);
+                }
             }
 
             // 按下 W 逆時針轉 90 度
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
             {
                 // 屬性面板上面的 rotation 必須用 eulerAngles 控制
-                currentTetris.eulerAngles += new Vector3(0, 0, 90);
+                currentTetris.eulerAngles += new Vector3Int(0, 0, 90);
+                
             }
             // 如果 玩家 按住 S 就加速
             if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
@@ -102,7 +118,7 @@ public class TetrisManager : MonoBehaviour
             #endregion
 
             // 如果 目前俄羅斯方塊 Y 軸 等於 -300 就 叫下一顆
-            if (currentTetris.anchoredPosition.y == -300)
+            if (currentTetris.anchoredPosition.y == -280)
             {
                 StartGame();
             }
@@ -117,6 +133,7 @@ public class TetrisManager : MonoBehaviour
     {
         // 下一顆編號 = 隨機 的 範圍(最小，最大) - 整數不會等於最大值
         indexNext = Random.Range(0, 7);
+
         // 下一個俄羅斯方塊區域 . 取得子物件(子物件編號) . 轉為遊戲物件 . 啟動設定(顯示)
         traNextArea.GetChild(indexNext).gameObject.SetActive(true);
     }
@@ -136,8 +153,8 @@ public class TetrisManager : MonoBehaviour
         GameObject current = Instantiate(tetris, traCanvas);
         // GetComponent<任何元件>()
         // <T> 泛型 - 指的是所有類型
-        // 目前俄羅斯方塊 . 取得元件<介面變形>() . 座標 = 二維向量
-        current.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 400);
+        // 目前俄羅斯方塊 . 取得元件<介面變形>() . 座標 = 生成座標陣列[編號]
+        current.GetComponent<RectTransform>().anchoredPosition = posSpawn[indexNext];
 
         // 2. 上一次俄羅斯方塊隱藏
         tetris.SetActive(false);
